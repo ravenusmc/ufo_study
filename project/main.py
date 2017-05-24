076f3f9995ca60630a2652daab8836594934cc3e
ravenusmc <mcuddy77@gmail.com>
2017-05-24 14:46:43 -0400

    added file for home page

--- project/main.py
+++ project/main.py
@@ -6,6 +6,7 @@ import pandas as pd
 
 #importing files that I made for this project
 from ufo import *
+from user import *
 
 #Setting up Flask
 app = Flask(__name__)
@@ -13,13 +14,14 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
+        pass
         #Recieving the information from the form from the user.
         username = request.form['username']
         password = request.form['password']
         #Creating the object that will represent the user.
-        user = User(username)
-    #     #Now checking to see if the user is in the database.
-    #     flag = user.check(username, password)
+        user = User()
+        #Now checking to see if the user is in the database.
+        flag = user.check(username, password)
     #     #Conditional statement to test if the user is a member of the site.
     #     if flag == True:
     #         #If the user is in the database, the user gets sent to the index page.
@@ -31,6 +33,10 @@ def login():
     #         #sign up page.
     #         return redirect(url_for('sign_up'))
     return render_template('login.html', title='Login Page')
+
+@app.route('/home')
+def home():
+    return render_template('home.html', title='Home Page')
 
 
 # set the secret key. keep this really secret:
--- project/user.py
+++ project/user.py
@@ -11,15 +11,65 @@ class User():
                                 database='people')
         self.cursor = self.conn.cursor()
 
-    def insert(self):
+    def insert(self, username, password):
         self._SQL = """insert into names
           (first_name, last_name)
           values
           (%s, %s)"""
-        f_name = input('Enter a First name: ')
-        l_name = input('Enter a last name: ')
-        self.cursor.execute(self._SQL, (f_name, l_name))
+        self.cursor.execute(self._SQL, (username, password))
         self.conn.commit()
 
-user = User()
-user.insert()
+    def query(self):
+        flag = False
+        query = ("""SELECT * FROM names WHERE first_name = %s""")
+        name = input('Enter First name: ')
+        self.cursor.execute(query, (name,))
+        # print(len(self.cursor.execute(query, (name,))))
+        print(self.cursor.rowcount)
+        if not self.cursor.rowcount:
+            print("No results found")
+            print(flag)
+        else:
+            for row in self.cursor:
+                print(row[0])
+                flag = True
+                print(flag)
+        # for row in self.cursor.fetchall():
+        #     print(row)
+        #     name = row
+        #     print(name)
+        #     input()
+        #     if name == ' ':
+        #         print('Nothing found!')
+        #     else:
+        #         print(name)
+
+# user = User()
+# user.query()
+
+
+    #This method will see if the user is actual user of the site.
+    # def check(self, username, password):
+    #     #I first encode the password to utf-8
+    #     password = password.encode('utf-8')
+    #     #I then search for a user that matches the username
+    #     user = self.db.users.find_one({
+    #         "username": username
+    #     });
+    #     #If user is not found then flag is set to False
+    #     if str(user) == 'None':
+    #         flag = False
+    #     #If the user is found, then another check is done to see if the hidden
+    #     #password matches the original one.
+    #     else:
+    #         #Setting the hashed variable to be used in the conditional statement.
+    #         hashed = user['password']
+    #         if bcrypt.hashpw(password, hashed) == hashed:
+    #             #I don't believe I need this user real. I only need to return
+    #             #the flag.
+    #             user_real = self.db.users.find_one({
+    #                 "username": username,
+    #                 "password": password
+    #             });
+    #             flag = True
+    #     return flag
